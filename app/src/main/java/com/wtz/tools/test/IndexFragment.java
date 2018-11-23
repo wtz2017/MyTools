e3a25b7f3e90266ae4269f5b72186d9e46d16664
wtz2017 <hhh1230937@163.com>
2018-11-23 15:58:54 +0800

    增加 AsyncTask 使用示例

--- app/src/main/java/com/wtz/tools/test/IndexFragment.java
+++ app/src/main/java/com/wtz/tools/test/IndexFragment.java
@@ -99,7 +99,7 @@ public class IndexFragment extends Fragment implements OnItemClickListener{
         mFragmentList.add(new FragmentItem("DrawerLayout", DrawerLayoutFragment.class.getName()));
         mFragmentList.add(new FragmentItem("GridViewLayout", GridViewFragment.class.getName()));
         mFragmentList.add(new FragmentItem("ViewPager", ViewPagerFragment.class.getName()));
-        mFragmentList.add(new FragmentItem("Seekbar", SeekbarFragment.class.getName()));
+        mFragmentList.add(new FragmentItem("Seekbar", SeekbarAsyncTaskFragment.class.getName()));
     }
 
     private void initView(View parent) {
