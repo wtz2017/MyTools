95a0e70201a6dc938d4e8d1413624fade5dfe31c
wtz2017 <hhh1230937@163.com>
2019-01-24 11:53:19 +0800

    增加浮窗的使用演示

--- app/src/main/java/com/wtz/tools/test/MainActivity.java
+++ app/src/main/java/com/wtz/tools/test/MainActivity.java
@@ -3,8 +3,16 @@ package com.wtz.tools.test;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.wtz.tools.R;
 import com.wtz.tools.receiver.AppReceiver;
@@ -25,6 +33,9 @@ public class MainActivity extends Activity {
     private Disposable mDisposableEvent1;
     private Disposable mDisposableEvent2;
     private Disposable mDisposableEvent3;
+
+    private WindowManager mWindowManager;
+    private ImageView mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +68,8 @@ public class MainActivity extends Activity {
                 Log.d(TAG, "RxBusRelay accept FragmentItem: " + fragmentItem.toString());
             }
         });
+
+        createFloatWindow();
     }
 
     @Override
@@ -90,6 +103,7 @@ public class MainActivity extends Activity {
         RxBus.unregister(mDisposableEvent1);
         RxBusFlowable.unregister(mDisposableEvent2);
         RxBusRelay.unregister(mDisposableEvent3);
+        destroyFloatWindow();
         super.onDestroy();
     }
     
@@ -101,6 +115,44 @@ public class MainActivity extends Activity {
             transaction.commitAllowingStateLoss();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void createFloatWindow() {
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
+        //layoutParams.format = PixelFormat.TRANSPARENT;
+        layoutParams.format = PixelFormat.RGBA_8888;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        layoutParams.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.x = 100;
+        layoutParams.y = 100;
+
+        mContentView = new ImageView(this);
+        mContentView.setImageResource(android.R.drawable.ic_menu_add);
+        mContentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d(TAG, "onTouch action：" + event.getAction());
+                if (MotionEvent.ACTION_DOWN == event.getAction()) {
+                    // TODO do something
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+
+        mWindowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager.addView(mContentView, layoutParams);
+    }
+
+    private void destroyFloatWindow() {
+        if (mWindowManager != null && mContentView != null) {
+            mWindowManager.removeView(mContentView);
         }
     }
 
