9b2788e01ec9857760ca8c59133103028acd2196
wtz2017 <hhh1230937@163.com>
2019-08-04 22:57:08 +0800

    增加：泛型示例

--- app/src/main/java/com/wtz/tools/utils/ReflectUtil.java
+++ app/src/main/java/com/wtz/tools/utils/ReflectUtil.java
@@ -1,11 +1,44 @@
 package com.wtz.tools.utils;
 
+import android.util.Log;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 
 public class ReflectUtil {
+    private static final String TAG = "ReflectUtil";
+
+    /**
+     * 对于类似如下获取泛型参数类型的Class的条件是：
+     * 泛型参数类型必须是明确指定的具体类型，不可以是 TypeVariable
+     * (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[index]
+     * (Class<T>) ((ParameterizedType) getClass().getGenericInterfaces()[index]).getActualTypeArguments()[index]
+     *
+     * 测试泛型结果：
+     * A-->getGenericInterfaces: interface com.wtz.tools.utils.ReflectUtil$IA1 is Class
+     * A-->getGenericInterfaces: com.wtz.tools.utils.ReflectUtil$IA2<java.lang.Boolean> is ParameterizedType
+     *   ParameterizedType info is:
+     *      getRawType: interface com.wtz.tools.utils.ReflectUtil$IA2
+     *      getOwnerType: class com.wtz.tools.utils.ReflectUtil
+     *      getActualTypeArguments: class java.lang.Boolean(Class);
+     * A-->getGenericInterfaces: com.wtz.tools.utils.ReflectUtil$IA3<java.lang.String, T> is ParameterizedType
+     *   ParameterizedType info is:
+     *      getRawType: interface com.wtz.tools.utils.ReflectUtil$IA3
+     *      getOwnerType: class com.wtz.tools.utils.ReflectUtil
+     *      getActualTypeArguments: class java.lang.String(Class);T(TypeVariable);
+     * B getGenericSuperclass:com.wtz.tools.utils.ReflectUtil$A<java.lang.Integer>
+     * B tClass:class java.lang.Integer
+     */
+    void testGeneric() {
+        new A<Long>().print();
+        new B().print();
+    }
 
     public static Class<?> getClass(String className) {
         Class<?> clazz = null;
@@ -94,12 +127,12 @@ public class ReflectUtil {
     }
 
     public static Object invokeStaticMethod(Class<?> clazz, String methodName, Class[] argTypes,
-            Object[] argValues, boolean hasReturnValue) {
+                                            Object[] argValues, boolean hasReturnValue) {
         return invokeMethod(clazz, null, methodName, argTypes, argValues, hasReturnValue);
     }
 
     public static Object invokeMethod(Class<?> clazz, Object instance, String methodName,
-            Class[] argTypes, Object[] argValues, boolean hasReturnValue) {
+                                      Class[] argTypes, Object[] argValues, boolean hasReturnValue) {
         Object result = null;
 
         Method m;
@@ -127,9 +160,10 @@ public class ReflectUtil {
 
         return result;
     }
-    
+
     /**
      * 循环查找当前类及父类中目标方法
+     *
      * @param targetClass
      * @param methodName
      * @param parameterTypes
@@ -171,4 +205,67 @@ public class ReflectUtil {
         }
         return result;
     }
+
+    interface IA1 {
+    }
+
+    interface IA2<T> {
+    }
+
+    interface IA3<K, V> {
+    }
+
+    class A<T> implements IA1, IA2<Boolean>, IA3<String, T> {
+        void print() {
+            Type[] types = getClass().getGenericInterfaces();
+            StringBuilder builder = new StringBuilder();
+            for (Type type : types) {
+                Log.d(TAG, "A-->getGenericInterfaces: " + type + " is " + getTypeName(type));
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pt = (ParameterizedType) type;
+                    builder.setLength(0);
+                    builder.append(" ParameterizedType info is:\n");
+                    builder.append(" getRawType: " + pt.getRawType() + "\n");
+                    builder.append(" getOwnerType: " + pt.getOwnerType() + "\n");
+                    builder.append(" getActualTypeArguments: ");
+                    Type[] atas = pt.getActualTypeArguments();
+                    for (Type aType : atas) {
+                        builder.append(aType);
+                        builder.append("(");
+                        builder.append(getTypeName(aType));
+                        builder.append(")");
+                        builder.append(";");
+                    }
+                    Log.d(TAG, builder.toString());
+                }
+            }
+        }
+    }
+
+    class B extends A<Integer> {
+        void print() {
+            Type type = getClass().getGenericSuperclass();
+            Log.d(TAG, "B getGenericSuperclass:" + type);
+            if (type instanceof ParameterizedType) {
+                Class<?> tClass = (Class<?>) ((ParameterizedType) type)
+                        .getActualTypeArguments()[0];
+                Log.d(TAG, "B tClass:" + tClass);
+            }
+        }
+    }
+
+    public static String getTypeName(Type type) {
+        String name = "unknown";
+        if (type instanceof Class) {
+            name = "Class";
+        } else if (type instanceof ParameterizedType) {
+            name = "ParameterizedType";
+        } else if (type instanceof GenericArrayType) {
+            name = "GenericArrayType";
+        } else if (type instanceof TypeVariable) {
+            name = "TypeVariable";
+        }
+        return name;
+    }
+
 }
