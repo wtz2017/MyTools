6bb5871fc910eb91d9ababd0c899aabf573a7df8
wtz2017 <hhh1230937@163.com>
2019-11-30 00:30:14 +0800

    Revert "优化代码"
    This reverts commit 3182043e

--- app/src/main/java/com/wtz/tools/utils/DateTimeUtil.java
+++ app/src/main/java/com/wtz/tools/utils/DateTimeUtil.java
@@ -100,13 +100,10 @@ public class DateTimeUtil {
      * @return
      */
     public static String changeRemainTimeToHms(long timeMilli) {
-        if (timeMilli == 0) {
-            return "00:00:00";// 避免 Math.round 参数遇到 0f 崩溃问题
-        }
-        int totalSeconds = Math.round(timeMilli / 1000f);// 毫秒数转秒数，毫秒部分四舍五入
-        int second = totalSeconds % 60;// 秒数除60得分钟数再取余得秒数
-        int minute = totalSeconds / 60 % 60;// 秒数除两个60得小时再取余得分钟数
-        int hour = totalSeconds / 60 / 60;// 秒数除两个60得小时数
+        timeMilli = timeMilli / 1000;// 毫秒数转秒数
+        int second = (int) (timeMilli % 60);// 秒数除60得分钟数再取余得秒数
+        int minute = (int) (timeMilli / 60 % 60);// 秒数除两个60得小时再取余得分钟数
+        int hour = (int) (timeMilli / 60 / 60);// 秒数除两个60得小时数
         String hourString = formatTime(String.valueOf(hour));
         String minuteString = formatTime(String.valueOf(minute));
         String secondString = formatTime(String.valueOf(second));
